3a93974349080208f49004b42cfd1ff33537959c
LeeVaughn <dlvprogramming@gmail.com>
2019-08-07 12:48:24 -0700

    updated code through Handle Validation Errors lesson

--- db/models/movie.js
+++ db/models/movie.js
@@ -5,7 +5,60 @@ module.exports = (sequelize) => {
   // defines a new table in the database
   // the first arg is an object literal defining the model attributes, the second is an object literal that sets the model options
   Movie.init({
-    title: Sequelize.STRING,
+    // set custom primary key column
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please provide a value for 'title'",
+        },
+        notEmpty: {
+          msg: "Please provide a value for 'title'",
+        }
+      },
+    },
+    runtime: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please provide a value for 'runtime'",
+        },
+        min: {
+          args: 1,
+          msg: "Please provide a value greater than '0' for 'runtime'",
+        },
+      },
+    },
+    releaseDate: {
+      type: Sequelize.DATEONLY,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please provide a value for 'releaseDate'",
+        },
+        isAfter: {
+          args: "1895-12-3-27",
+          msg: "Please provide a value on or after '1895-12-28' for 'releaseDate'",
+        },
+      },
+    },
+    isAvailableOnVHS: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+      validate: {
+        notNull: {
+          msg: "Please provide a value for 'title'",
+        },
+      },
+    },
   }, { sequelize }); // same as { sequelize: sequelize }
 
   return Movie;
